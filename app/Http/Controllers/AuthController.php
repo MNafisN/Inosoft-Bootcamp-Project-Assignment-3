c6de79f28966aec4eeb6a5cdd716a166f662ee0c
MNafisN <naf1s.n4ufally@gmail.com>
2023-05-16 19:22:49 +0700

    Tambah Fungsi Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,31 +3,103 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use MongoDB\Exception\InvalidArgumentException;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function __construct()
     {
-        $credentials = request(['email', 'password']);
-        $token = auth()->attempt($credentials);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    /**
+     * Register a new user
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->only('name', 'email', 'password'), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'email_verified_at' => time(),
+        ]);
+
+        return response()->json([
+            'status' => 201,
+            'message' => 'User created successfully',
+            'user' => $user
+        ], 201);
+    }
+
+    /**
+     * Log in and get a JWT token via given credentials
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $validator = Validator::make($credentials, [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
+        }        
+
+        $token = auth()->attempt($credentials, true);
 
         if (!$token) {
             return response()->json([
+                'status' => 401,
                 'error' => 'Unauthorized'
             ], 401);
         }
 
         return response()->json([
+            'status' => 200,
+            'user' => auth()->user(),
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ], 200);
     }
 
+    /**
+     * Get current logged in user's data
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function data()
     {
         return response()->json(auth()->user(), 200);
     }
+
+    /**
+     * Get a new JWT token for the logged in user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
 
     public function refresh()
     {
@@ -38,6 +110,11 @@ class AuthController extends Controller
         ], 200);
     }
 
+    /**
+     * Log the user out (Invalidate the token)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function logout()
     {
         auth()->logout();
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\TodoController;
 Route::group([
     'prefix' => 'auth'
 ], function() {
+    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::group([
         'middleware' => 'auth:api'
